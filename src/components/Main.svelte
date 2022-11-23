4f94b3fd2d204b0924f87867076d745c0cf5a5f7
Santiago Pintos <spintos@outlook.es>
2022-11-23 18:48:36 -0300

    Estructura
    Signed-off-by: Santiago Pintos <spintos@outlook.es>

---
+++ src/components/Main.svelte
@@ -0,0 +1,6 @@
+<div class="Main">
+    <div class="Main-container">
+        <!-- slot permite insertar elementos hijos que le paso al componente -->
+        <slot />
+    </div>
+</div>
--- src/containers/App.svelte
+++ src/containers/App.svelte
@@ -1,10 +1,19 @@
+<script>
+    import Header from './../components/Header.svelte';
+    import Main from './../components/Main.svelte';
+    import TimeLine from './../components/Timeline.svelte';
+    import Sidebar from './../components/Sidebar.svelte';
+</script>
+
 <style>
     :global(body){
         background-color: #676778;
         color: white;
     }
 </style>
+<Header />
+<Main>
+    <TimeLine />
+    <Sidebar />
+</Main>
 
-<div class="main">
-    <h1>Hello Pugstagram</h1>
-</div>
