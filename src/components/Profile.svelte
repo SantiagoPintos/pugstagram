6178a351cdfbc4612c53d992db3e7df32e8ee0a8
Santiago Pintos <spintos@outlook.es>
2022-11-24 14:48:42 -0300

    Componentes y Sidebar
    Signed-off-by: Santiago Pintos <spintos@outlook.es>

---
+++ src/components/Profile.svelte
@@ -0,0 +1,11 @@
+<div class="Profile">
+    <div class="Profile-content">
+        <div class="Profile-avatar">
+            <img src="https://imageio.forbes.com/specials-images/imageserve/62d599ede3ff49f348f9b9b4/0x0.jpg?format=jpg&crop=821,821,x155,y340,safe&height=416&width=416&fit=bounds" alt="profile">
+        </div>
+        <div class="Profile-info">
+            <h2>@billgates</h2>
+            <span>Bill Gates</span>
+        </div>
+    </div>
+</div>
---
+++ src/components/Stories.svelte
@@ -0,0 +1,17 @@
+<div class="Stories">
+    <div class="Stories-container">
+        <div class="Stories-head">
+            <h2>Historias</h2>
+            <span>Ver todas</span>
+        </div>
+        <div class="Stories-items">
+            <div class="Stories-item">
+                <div class="Stories-item-box">
+                    <img src="https://imageio.forbes.com/specials-images/imageserve/62d599ede3ff49f348f9b9b4/0x0.jpg?format=jpg&crop=821,821,x155,y340,safe&height=416&width=416&fit=bounds" alt="story">
+                </div>
+                <h2>billgates</h2>
+                <span>Hace 2hs</span>
+            </div>
+        </div>
+    </div>
+</div>
