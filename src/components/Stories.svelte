38ed488eb736410b76fb3b93934976e0a743e2e1
Santiago Pintos <spintos@outlook.es>
2022-11-24 20:28:03 -0300

    Estilos
    Signed-off-by: Santiago Pintos <spintos@outlook.es>

--- src/components/Stories.svelte
+++ src/components/Stories.svelte
@@ -1,3 +1,66 @@
+<script>
+
+</script>
+
+<style>
+    .Stories {
+      border: 1px solid rgba(219, 219, 219, 1);
+      border-radius: 4px;
+      margin: 1em 0;
+      padding: 0.5em 1em;
+      background-color: white;
+    }
+    .Stories-item img {
+      width: 32px;
+      height: 32px;
+      border-radius: 50%;
+    }
+    .Stories-item {
+      display: flex;
+      align-items: center;
+      margin: 0 0 0.5em 0;
+    }
+    .Stories-item h2 {
+      font-size: 14px;
+      color: black;
+      margin: 0 0 0 0.5em;
+    }
+    .Stories-item h2 span {
+      display: block;
+      font-size: 9px;
+      text-transform: uppercase;
+      color: gray;
+    }
+    .Stories-head {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding: 0.5em 0 0 0;
+    }
+    .Stories-head h2 {
+      font-size: 12px;
+      font-weight: 600;
+    }
+    .Stories-head span {
+      font-size: 12px;
+      font-weight: normal;
+      color: black;
+    }
+    .Stories-items {
+      margin: 0.5em 0 0.5em 0;
+    }
+    .Stories-item-box {
+      width: 32px;
+      height: 32px;
+      border: double 2px transparent;
+      border-radius: 100%;
+      background-image: linear-gradient(#fff, #fff),
+        radial-gradient(circle at top left, #f09433, #bc1888);
+      background-origin: border-box;
+      background-clip: content-box, border-box;
+    }
+</style>
+
 <div class="Stories">
     <div class="Stories-container">
         <div class="Stories-head">
@@ -10,7 +73,7 @@
                     <img src="https://imageio.forbes.com/specials-images/imageserve/62d599ede3ff49f348f9b9b4/0x0.jpg?format=jpg&crop=821,821,x155,y340,safe&height=416&width=416&fit=bounds" alt="story">
                 </div>
                 <h2>billgates</h2>
-                <span>Hace 2hs</span>
+                <span>2 horas antes</span>
             </div>
         </div>
     </div>
